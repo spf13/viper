40762f75412949b579990808c29217ed19a4b320
Kiril Zvezdarov <kzvezdarov@monetate.com>
2015-03-28 13:36:55 -0400

    Current working directory is added to the config search paths by default

--- viper.go
+++ viper.go
@@ -143,6 +143,13 @@ func New() *Viper {
 	v.env = make(map[string]string)
 	v.aliases = make(map[string]string)
 
+	wd, err := os.Getwd()
+	if err != nil {
+		jww.INFO.Println("Could not add cwd to search paths", err)
+	} else {
+		v.AddConfigPath(wd)
+	}
+
 	return v
 }
 
