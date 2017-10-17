8d5bbde9533ec5b73631cb3c6e45116cb6de4379
Fabian Ruff <fabian@progra.de>
2017-10-17 11:33:56 +0200

    Fix retrieval of pflag stringArray
    `pflag.StringArray` suffers from the same problems as `StringSlice` did described in this issue https://github.com/spf13/viper/issues/112

--- viper.go
+++ viper.go
@@ -896,7 +896,7 @@ func (v *Viper) find(lcaseKey string) interface{} {
 			return cast.ToInt(flag.ValueString())
 		case "bool":
 			return cast.ToBool(flag.ValueString())
-		case "stringSlice":
+		case "stringSlice", "stringArray":
 			s := strings.TrimPrefix(flag.ValueString(), "[")
 			s = strings.TrimSuffix(s, "]")
 			res, _ := readAsCSV(s)
@@ -965,7 +965,7 @@ func (v *Viper) find(lcaseKey string) interface{} {
 			return cast.ToInt(flag.ValueString())
 		case "bool":
 			return cast.ToBool(flag.ValueString())
-		case "stringSlice":
+		case "stringSlice", "stringArray":
 			s := strings.TrimPrefix(flag.ValueString(), "[")
 			s = strings.TrimSuffix(s, "]")
 			res, _ := readAsCSV(s)
