be985b07ea22678d6ecc37852b3e9327e7fa96a6
kun <oiooj@qq.com>
2018-04-19 17:34:10 +0800

    fix dep wrong case

--- util.go
+++ util.go
@@ -20,7 +20,7 @@ import (
 
 	"github.com/spf13/afero"
 	"github.com/spf13/cast"
-	jww "github.com/spf13/jwalterweatherman"
+	jww "github.com/spf13/jWalterWeatherman"
 )
 
 // ConfigParseError denotes failing to parse configuration file.
