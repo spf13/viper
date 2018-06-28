907c19d40d9a6c9bb55f040ff4ae45271a4754b9
Chris Reeves <chris@thisissoon.com>
2018-06-28 10:55:33 +0100

    Support customising mapstructure.DecoderConfig for Unmarshal
    * Added a new `DecoderConfigOption` type allowing the user to write custom
      functions that can override the default mapstructure.DecoderConfig
      settings
    
    * Added a new `DecodeHook` function which returns
      a `DecoderConfigOption`. This allows the user to easily set their own
      Decode hooks when Unmarshaling
    
    * Updated Unmarshal, UnmarshalKey and defaultDecoderConfig to support variadic
      trailing `DecoderConfigOption` functions to allow for customisation of
      the default  mapstructure.DecoderConfig
    
    * Added a test case with example usage

--- viper_test.go
+++ viper_test.go
@@ -7,6 +7,7 @@ package viper
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -18,6 +19,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/afero"
 	"github.com/spf13/cast"
 
@@ -501,6 +503,42 @@ func TestUnmarshal(t *testing.T) {
 		t.Fatalf("unable to decode into struct, %v", err)
 	}
 	assert.Equal(t, &config{Name: "Steve", Port: 1234, Duration: time.Second + time.Millisecond}, &C)
+}
+
+func TestUnmarshalWithDecoderOptions(t *testing.T) {
+	Set("credentials", "{\"foo\":\"bar\"}")
+
+	opt := DecodeHook(mapstructure.ComposeDecodeHookFunc(
+		mapstructure.StringToTimeDurationHookFunc(),
+		mapstructure.StringToSliceHookFunc(","),
+		// Custom Decode Hook Function
+		func(rf reflect.Kind, rt reflect.Kind, data interface{}) (interface{}, error) {
+			if rf != reflect.String || rt != reflect.Map {
+				return data, nil
+			}
+			m := map[string]string{}
+			raw := data.(string)
+			if raw == "" {
+				return m, nil
+			}
+			return m, json.Unmarshal([]byte(raw), &m)
+		},
+	))
+
+	type config struct {
+		Credentials map[string]string
+	}
+
+	var C config
+
+	err := Unmarshal(&C, opt)
+	if err != nil {
+		t.Fatalf("unable to decode into struct, %v", err)
+	}
+
+	assert.Equal(t, &config{
+		Credentials: map[string]string{"foo": "bar"},
+	}, &C)
 }
 
 func TestBindPFlags(t *testing.T) {
