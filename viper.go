082cade898fda80a8e6cb88476e8209998e08bf5
TwiN <twin@twinnation.org>
2019-08-16 17:15:10 -0400

    Fix small typo

--- viper.go
+++ viper.go
@@ -3,7 +3,7 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 
-// Viper is a application configuration system.
+// Viper is an application configuration system.
 // It believes that applications can be configured a variety of ways
 // via flags, ENVIRONMENT variables, configuration files retrieved
 // from the file system, or a remote key/value store.
