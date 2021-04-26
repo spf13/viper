a7650330d7170d0b92651446fcb7e96a489f3071
vuca <aivuca@163.com>
2021-04-26 08:57:18 +0800

    Add SetViper() function

--- viper.go
+++ viper.go
@@ -781,6 +781,11 @@ func GetViper() *Viper {
 	return v
 }
 
+// SetViper sets the global Viper instance.
+func SetViper(vp *Viper) {
+	v=vp
+}
+
 // Get can retrieve any value given the key to use.
 // Get is case-insensitive for a key.
 // Get has the behavior of returning the value associated with the first
