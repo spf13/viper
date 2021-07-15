e1924e3858d59a2611312f8a469f3921003b2f78
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-07-16 01:47:03 +0200

    test(encoding): add tests for existing encoding implementations
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ internal/encoding/json/codec_test.go
@@ -0,0 +1,95 @@
+package json
+
+import (
+	"reflect"
+	"testing"
+)
+
+// encoded form of the data
+const encoded = `{
+  "key": "value",
+  "list": [
+    "item1",
+    "item2",
+    "item3"
+  ],
+  "map": {
+    "key": "value"
+  },
+  "nested_map": {
+    "map": {
+      "key": "value",
+      "list": [
+        "item1",
+        "item2",
+        "item3"
+      ]
+    }
+  }
+}`
+
+// Viper's internal representation
+var data = map[string]interface{}{
+	"key": "value",
+	"list": []interface{}{
+		"item1",
+		"item2",
+		"item3",
+	},
+	"map": map[string]interface{}{
+		"key": "value",
+	},
+	"nested_map": map[string]interface{}{
+		"map": map[string]interface{}{
+			"key": "value",
+			"list": []interface{}{
+				"item1",
+				"item2",
+				"item3",
+			},
+		},
+	},
+}
+
+func TestCodec_Encode(t *testing.T) {
+	codec := Codec{}
+
+	b, err := codec.Encode(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if encoded != string(b) {
+		t.Fatalf("decoded value does not match the expected one\nactual:   %#v\nexpected: %#v", string(b), encoded)
+	}
+}
+
+func TestCodec_Decode(t *testing.T) {
+	t.Run("OK", func(t *testing.T) {
+		codec := Codec{}
+
+		v := map[string]interface{}{}
+
+		err := codec.Decode([]byte(encoded), v)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !reflect.DeepEqual(data, v) {
+			t.Fatalf("decoded value does not match the expected one\nactual:   %#v\nexpected: %#v", v, data)
+		}
+	})
+
+	t.Run("InvalidData", func(t *testing.T) {
+		codec := Codec{}
+
+		v := map[string]interface{}{}
+
+		err := codec.Decode([]byte(`invalid data`), v)
+		if err == nil {
+			t.Fatal("expected decoding to fail")
+		}
+
+		t.Logf("decoding failed as expected: %s", err)
+	})
+}
