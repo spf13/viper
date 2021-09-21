558a299a016fe18afc4d9f594a86af41af514e64
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-09-21 11:20:25 +0200

    feat: use TB.Setenv on Go 1.17
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- internal/testutil/env.go
+++ internal/testutil/env.go
@@ -9,15 +9,6 @@ import (
 // Licensed under the MIT license
 // Copyright (c) 2017 Canonical Ltd.
 
-// Setenv sets an environment variable to a temporary value for the
-// duration of the test.
-//
-// At the end of the test (see "Deferred execution" in the package docs), the
-// environment variable is returned to its original value.
-func Setenv(t *testing.T, name, val string) {
-	setenv(t, name, val, true)
-}
-
 // Unsetenv unsets an environment variable for the duration of a test.
 func Unsetenv(t *testing.T, name string) {
 	setenv(t, name, "", false)
