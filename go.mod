76d0539b90f758ee34aabbaea77b139d679dfc45
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-09-12 00:50:11 +0200

    refactor: drop unused jww code
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- go.mod
+++ go.mod
@@ -9,9 +9,9 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/pelletier/go-toml/v2 v2.1.0
 	github.com/sagikazarmark/crypt v0.14.0
+	github.com/sagikazarmark/slog-shim v0.1.0
 	github.com/spf13/afero v1.9.5
 	github.com/spf13/cast v1.5.1
-	github.com/spf13/jwalterweatherman v1.1.0
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.8.4
 	github.com/subosito/gotenv v1.6.0
@@ -58,7 +58,6 @@ require (
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
-	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	go.etcd.io/etcd/api/v3 v3.5.9 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.9 // indirect
 	go.etcd.io/etcd/client/v2 v2.305.9 // indirect
--- logger.go
+++ logger.go
@@ -2,10 +2,8 @@ package viper
 
 import (
 	"context"
-	"fmt"
 
 	slog "github.com/sagikazarmark/slog-shim"
-	jww "github.com/spf13/jwalterweatherman"
 )
 
 // Logger is a unified interface for various logging use cases and practices, including:
@@ -42,42 +40,6 @@ type Logger interface {
 	// Loggers commonly provide Fatal and Panic levels above Error level,
 	// but exiting and panicking is out of scope for a logging library.
 	Error(msg string, keyvals ...interface{})
-}
-
-type jwwLogger struct{}
-
-func (jwwLogger) Trace(msg string, keyvals ...interface{}) {
-	jww.TRACE.Printf(jwwLogMessage(msg, keyvals...))
-}
-
-func (jwwLogger) Debug(msg string, keyvals ...interface{}) {
-	jww.DEBUG.Printf(jwwLogMessage(msg, keyvals...))
-}
-
-func (jwwLogger) Info(msg string, keyvals ...interface{}) {
-	jww.INFO.Printf(jwwLogMessage(msg, keyvals...))
-}
-
-func (jwwLogger) Warn(msg string, keyvals ...interface{}) {
-	jww.WARN.Printf(jwwLogMessage(msg, keyvals...))
-}
-
-func (jwwLogger) Error(msg string, keyvals ...interface{}) {
-	jww.ERROR.Printf(jwwLogMessage(msg, keyvals...))
-}
-
-func jwwLogMessage(msg string, keyvals ...interface{}) string {
-	out := msg
-
-	if len(keyvals) > 0 && len(keyvals)%2 == 1 {
-		keyvals = append(keyvals, nil)
-	}
-
-	for i := 0; i <= len(keyvals)-2; i += 2 {
-		out = fmt.Sprintf("%s %v=%v", out, keyvals[i], keyvals[i+1])
-	}
-
-	return out
 }
 
 // WithLogger sets a custom logger.
