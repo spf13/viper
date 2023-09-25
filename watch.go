6a30cbb1bcf30c2923412c560412e8da97ff7507
Oleksandr Redko <Oleksandr_Redko@epam.com>
2023-09-25 13:51:41 +0300

    chore: remove deprecated build tags

--- watch.go
+++ watch.go
@@ -1,5 +1,4 @@
 //go:build darwin || dragonfly || freebsd || openbsd || linux || netbsd || solaris || windows
-// +build darwin dragonfly freebsd openbsd linux netbsd solaris windows
 
 package viper
 
