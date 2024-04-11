92882dd5595bb39d19aae8d87c75303222eccfa4
rajatm99 <iamrajatm99@gmail.com>
2024-04-11 15:18:44 +0530

    added a file exists check before reading

--- viper.go
+++ viper.go
@@ -1642,6 +1642,11 @@ func (v *Viper) ReadInConfig() error {
 		return UnsupportedConfigError(v.getConfigType())
 	}
 
+	fileExist, err := afero.Exists(v.fs, filename)
+	if !fileExist {
+		return ConfigFileNotFoundError{filename, fmt.Sprintf("%s", v.configPaths)}
+	}
+
 	v.logger.Debug("reading file", "file", filename)
 	file, err := afero.ReadFile(v.fs, filename)
 	if err != nil {
