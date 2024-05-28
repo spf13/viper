537f03e453bf188478839e8d101c2c23e6478906
Maxime mouial <maxime.mouial@gmail.com>
2024-05-28 17:30:39 +0200

    Revert "Revert "Fix Viper 'UnmarshalKey' behavior to include all sources""
    This reverts commit bc50f827eb3260168a72a9debb29d962adfb87c3.
    
    Now that key lookup has been fixed by
    7d76daf975dc0db59c2e4193e732a70513d4468a, we can re-introduce the fix
    for unmarshalkey.

--- viper.go
+++ viper.go
@@ -865,13 +865,27 @@ func UnmarshalKey(key string, rawVal interface{}, opts ...DecoderConfigOption) e
 	return v.UnmarshalKey(key, rawVal, opts...)
 }
 func (v *Viper) UnmarshalKey(key string, rawVal interface{}, opts ...DecoderConfigOption) error {
-	err := decode(v.Get(key), defaultDecoderConfig(rawVal, opts...))
+	lcaseKey := strings.ToLower(key)
 
-	if err != nil {
-		return err
+	// AllSettings returns settings from every sources merged into one tree
+	settings := v.AllSettings()
+
+	keyParts := strings.Split(lcaseKey, v.keyDelim)
+	for i := 0; i < len(keyParts)-1; i++ {
+		if value, found := settings[keyParts[i]]; found {
+			if valueMap, ok := value.(map[string]interface{}); ok {
+				settings = valueMap
+				continue
+			}
+			// if the current value is not a map[string]interface{} we most likely reach a
+			// leaf and the key/path is wrong
+			return fmt.Errorf("unknown key %s", lcaseKey)
+		} else {
+			return fmt.Errorf("unknown key %s", lcaseKey)
+		}
 	}
-
-	return nil
+	finalSetting := settings[keyParts[len(keyParts)-1]]
+	return decode(finalSetting, defaultDecoderConfig(rawVal, opts...))
 }
 
 // Unmarshal unmarshals the config into a Struct. Make sure that the tags
