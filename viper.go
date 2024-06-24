9a962379e65aa18f76dcb18117d432b84a11083b
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-06-24 18:14:40 +0200

    fix: do not allow overwriting the mapstructure output
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- viper.go
+++ viper.go
@@ -945,7 +945,6 @@ func (v *Viper) decodeStructKeys(input any, opts ...DecoderConfigOption) ([]stri
 func defaultDecoderConfig(output any, opts ...DecoderConfigOption) *mapstructure.DecoderConfig {
 	c := &mapstructure.DecoderConfig{
 		Metadata:         nil,
-		Result:           output,
 		WeaklyTypedInput: true,
 		DecodeHook: mapstructure.ComposeDecodeHookFunc(
 			mapstructure.StringToTimeDurationHookFunc(),
@@ -953,9 +952,14 @@ func defaultDecoderConfig(output any, opts ...DecoderConfigOption) *mapstructure
 			stringToWeakSliceHookFunc(","),
 		),
 	}
+
 	for _, opt := range opts {
 		opt(c)
 	}
+
+	// Do not allow overwriting the output
+	c.Result = output
+
 	return c
 }
 
