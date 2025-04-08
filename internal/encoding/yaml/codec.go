2cf9fc25ac9e17b920aa0f57b2d3725666b1f8c1
William Crum <will@wcrum.dev>
2025-04-08 10:19:47 -0600

    fix: move to sigs.k8s.io/yaml/goyaml.v3

--- internal/encoding/yaml/codec.go
+++ internal/encoding/yaml/codec.go
@@ -1,6 +1,6 @@
 package yaml
 
-import "sigs.k8s.io/yaml"
+import yaml "sigs.k8s.io/yaml/goyaml.v3"
 
 // Codec implements the encoding.Encoder and encoding.Decoder interfaces for YAML encoding.
 type Codec struct{}
