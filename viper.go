36e570a0fa77407031047f4332b2100579049111
Matthew Coleman <matthew.e.coleman@gmail.com>
2025-06-15 17:35:14 -0400

    fix: return not found error if setting config

--- viper.go
+++ viper.go
@@ -76,7 +76,12 @@ type ConfigFileNotFoundError struct {
 
 // Error returns the formatted configuration error.
 func (fnfe ConfigFileNotFoundError) Error() string {
-	return fmt.Sprintf("Config File %q Not Found in %q", fnfe.name, fnfe.locations)
+	message := fmt.Sprintf("Config file %q Not Found", fnfe.name)
+	if fnfe.locations != "" {
+		message += fmt.Sprintf(" in %q", fnfe.locations)
+	}
+
+	return message
 }
 
 // ConfigFileAlreadyExistsError denotes failure to write new configuration file.
@@ -1527,11 +1532,17 @@ func (v *Viper) ReadInConfig() error {
 	}
 
 	v.logger.Debug("reading file", "file", filename)
+	exists, err := afero.Exists(v.fs, filename)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return ConfigFileNotFoundError{name: filename, locations: ""}
+	}
 	file, err := afero.ReadFile(v.fs, filename)
 	if err != nil {
 		return err
 	}
-
 	config := make(map[string]any)
 
 	err = v.unmarshalReader(bytes.NewReader(file), config)
