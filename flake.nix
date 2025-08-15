eb5b99fe213f5ea25aa86ad5a6fedc32f9df3b36
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2025-08-16 01:28:19 +0200

    build: update dev env
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -7,51 +7,55 @@
     devenv.url = "github:cachix/devenv";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs =
+    inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.devenv.flakeModule
       ];
 
-      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-      perSystem = { config, self', inputs', pkgs, system, ... }: rec {
-        devenv.shells = {
-          default = {
-            languages = {
-              go.enable = true;
-              go.package = pkgs.go_1_23;
-            };
+      perSystem =
+        { pkgs, ... }:
+        {
+          devenv.shells = {
+            default = {
+              languages = {
+                go.enable = true;
+              };
 
-            pre-commit.hooks = {
-              nixpkgs-fmt.enable = true;
-              yamllint.enable = true;
-            };
+              git-hooks.hooks = {
+                nixpkgs-fmt.enable = true;
+                yamllint.enable = true;
+              };
 
-            packages = with pkgs; [
-              gnumake
+              packages = with pkgs; [
+                gnumake
 
-              golangci-lint
-              yamllint
-            ];
+                golangci-lint
+                yamllint
+              ];
 
-            scripts = {
-              versions.exec = ''
-                go version
-                golangci-lint version
+              scripts = {
+                versions.exec = ''
+                  go version
+                  golangci-lint version
+                '';
+              };
+
+              enterShell = ''
+                versions
               '';
+
+              # https://github.com/cachix/devenv/issues/528#issuecomment-1556108767
+              containers = pkgs.lib.mkForce { };
             };
-
-            enterShell = ''
-              versions
-            '';
-
-            # https://github.com/cachix/devenv/issues/528#issuecomment-1556108767
-            containers = pkgs.lib.mkForce { };
           };
-
-          ci = devenv.shells.default;
         };
-      };
     };
 }
