bb09bc6c2f430912691608b33e61c7b641ef1270
ccoVeille <3875889+ccoVeille@users.noreply.github.com>
2025-08-16 10:11:39 +0200

    doc: add documentation to all exported methods

--- internal/encoding/json/codec.go
+++ internal/encoding/json/codec.go
@@ -7,11 +7,13 @@ import (
 // Codec implements the encoding.Encoder and encoding.Decoder interfaces for JSON encoding.
 type Codec struct{}
 
+// Encode encodes a map[string]any into a JSON byte slice.
 func (Codec) Encode(v map[string]any) ([]byte, error) {
 	// TODO: expose prefix and indent in the Codec as setting?
 	return json.MarshalIndent(v, "", "  ")
 }
 
+// Decode decodes a JSON byte slice into a map[string]any.
 func (Codec) Decode(b []byte, v map[string]any) error {
 	return json.Unmarshal(b, &v)
 }
